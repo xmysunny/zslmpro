e087fc369bf3bf497e3c48b18abe87ca6a924280
岸 <510838021>
2020-06-09 14:38:12 +0800

    江岸提交测试2

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 //chenzheng
-//你好
+//你好啊
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
