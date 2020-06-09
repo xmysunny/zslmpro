45b441b2d078e4f93ba024a6ce8448f2938ef151
Administrator <1457592194@qq.com>
2020-06-09 17:15:12 +0800

    更新了实体类-殷俊

---
+++ common/src/main/java/com/example/common/pojo/menu.java
@@ -0,0 +1,34 @@
+package com.example.common.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @BelongsProject: zslmpro
+ * @BelongsPackage: com.example.common.pojo
+ * @Author: 殷俊
+ * @CreateTime: 2020-06-09 17:13
+ * @Description: 菜单
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class menu {
+    /** id */
+    private Integer id ;
+    /** 菜单名称 */
+    private String menuName ;
+    /** 父id */
+    private Integer parentId ;
+    /** 图标 */
+    private String iconcls ;
+    /** 地址 */
+    private String url ;
+    /** 创建时间 */
+    private Date createdTime ;
+    /** 更新时间 */
+    private Date updatedTime ;
+}
---
+++ common/src/main/java/com/example/common/pojo/menuRole.java
@@ -0,0 +1,30 @@
+package com.example.common.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @BelongsProject: zslmpro
+ * @BelongsPackage: com.example.common.pojo
+ * @Author: 殷俊
+ * @CreateTime: 2020-06-09 17:14
+ * @Description: 角色菜单关联
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class menuRole {
+    /** id */
+    private Integer id ;
+    /** 角色id */
+    private Integer roleId ;
+    /** 菜单id */
+    private Integer menuId ;
+    /** 创建时间 */
+    private Date createdTime ;
+    /** 更新时间 */
+    private Date updatedTime ;
+}
