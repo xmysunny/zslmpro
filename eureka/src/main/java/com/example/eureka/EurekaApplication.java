7b5d6345616db2430628984f52c072cdb04dc3ba
cz6991 <wjcz783577172@163.com>
2020-06-09 14:33:36 +0800

    写了个注释

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -3,6 +3,7 @@ package com.example.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//chenzheng
 
 @EnableEurekaServer
 @SpringBootApplication
