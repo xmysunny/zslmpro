4adaa0cbf00ae0c7a32a4f68a6894b6f553ed3b8
岸 <510838021>
2020-06-09 20:23:23 +0800

    江岸提交product2

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 //@MapperScan("com.example.user.mapper")
+//
 @EnableEurekaClient
 @SpringBootApplication
 public class UserApplication {
