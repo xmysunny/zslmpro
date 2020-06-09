42e83d83a80f94094d92964f96366de7127eaf78
99177 <991776525@qq.com>
2020-06-09 15:34:49 +0800

    启动成功

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -3,10 +3,8 @@ package com.example.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-//chenzhengfangjing
-//jjjj
-//chenzheng
-////---------
+
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
